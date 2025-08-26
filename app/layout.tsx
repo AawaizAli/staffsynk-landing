55c9b82a4f2f873c7b6277763c9bf83dba6fe2e9
Aawaiz <aawaizali8@gmail.com>
2025-08-26 21:21:06 +0500

    name description

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,8 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.app',
+  title: 'StaffSynk - Simplified Staff Management',
+  description: 'A product of Synkflow Technologies',
 }
 
 export default function RootLayout({
